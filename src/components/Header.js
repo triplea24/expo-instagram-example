4fb50892039c6208b7dfe3fdabc2d1266270fae0
Amir Ahangari <triplea1373@gmail.com>
2018-07-10 12:41:01 +0430

    Convert Header from Class-based to functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,14 @@
 import React,{Component} from 'react';
 import {View,Text} from 'react-native';
 
-export default class Header extends Component{
-    render(){
-        let {title} = this.props;
-        if(!title)title = 'header';
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>{title}</Text>
-            </View>
-        );
-    }
+const Header = (props) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{props.title}</Text>
+        </View>
+    );
 }
+
 const styles = {
     container: {
         backgroundColor: '#f8f8f8',
@@ -27,3 +24,4 @@ const styles = {
         margin: 13,
     }
 }
+export default Header;
