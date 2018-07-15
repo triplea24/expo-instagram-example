c50674b8ed321fbdcadced68971622d15c996049
Amir Ahangari <triplea1373@gmail.com>
2018-07-15 18:10:18 +0430

    Remove unnecessary bugs and modify Post component's style

--- src/components/Post.js
+++ src/components/Post.js
@@ -32,36 +32,43 @@ export default class Post extends Component{
                     </View>
                 </View>
                 <PostPhoto uri={uri} />
-                <View style={styles.likeContainer}>
-                    <TouchableWithoutFeedback onPress={this.toggleLike.bind(this)}>
-                        <Ionicons
-                            name={this.state.likeIcon}
-                            size={30}
-                            style={{color: this.state.liked ? 'red' : 'black'}}
-                        />
-                    </TouchableWithoutFeedback>
-                </View>
-                <View style={styles.imageMeta}>
-                    <Text style={styles.username}>{username}</Text>
-                    <Text>{caption}</Text>
+                <View style={styles.postMetaContainer}>
+                    <View style={styles.likeContainer}>
+                        <TouchableWithoutFeedback onPress={this.toggleLike.bind(this)}>
+                            <Ionicons
+                                name={this.state.likeIcon}
+                                size={30}
+                                style={{color: this.state.liked ? 'red' : 'black'}}
+                            />
+                        </TouchableWithoutFeedback>
+                    </View>
+                    <View style={styles.imageMeta}>
+                        <Text style={styles.username}>{username}</Text>
+                        <Text>{caption}</Text>
+                    </View>
                 </View>
             </View>
         );
     }
 }
+
+const padding = 10;
+
 const styles = StyleSheet.create({
     container: {
-        margin: 10,
     },
     thumbnailContainer: {
         flexDirection: 'row',
-        padding: 5,
+        padding: padding,
         borderBottomColor: "#ddd",
         borderBottomWidth: 1,
     },
     userContainer: {
         justifyContent: 'center',
-        padding: 10,
+        padding: padding,
+    },
+    postMetaContainer: {
+        padding: padding,
     },
     imageMeta: {
         flexDirection: 'row',
