b2e4650f9c5535b298dbc576c035133bfd718889
Amir Ahangari <triplea1373@gmail.com>
2018-07-15 17:10:25 +0430

    Add reducers and store to the project

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { KeepAwake, registerRootComponent } from 'expo';
 import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { Header } from './src/components';
+import { FeedScreen } from './src/screens';
+import reducers from './src/reducers';
 
 if (__DEV__) {
   KeepAwake.activate();
 }
 
-
-import { Header } from './src/components';
-import { FeedScreen } from './src/screens';
+const store = createStore(reducers);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Header title="Packt Instagram"/>
-      <FeedScreen />
-      </View>
+      <Provider store={store}>
+        <View>
+          <Header title="Packt Instagram"/>
+          <FeedScreen />
+        </View>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers, createStore } from 'redux';
+
+export default reducers = combineReducers({
+    photo: () => [],
+});
+
